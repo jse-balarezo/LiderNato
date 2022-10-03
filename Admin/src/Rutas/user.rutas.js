cff507e86a3665e171be8c2b93651d4b9b46ffb4
yavbot <73229786+MiguelChoez@users.noreply.github.com>
2022-10-03 03:53:47 -0500

    combios

--- Admin/src/Rutas/user.rutas.js
+++ Admin/src/Rutas/user.rutas.js
@@ -2,15 +2,8 @@ const express = require("express");
 
 const router = express.Router();
 const {
-  createUser,
   getUsers,
-  renderHome,
-  getRegister,
 } = require("../controladores/usuario.controlador");
 
-router.get("/", renderHome);
-router.get("/get-register", getRegister);
-router.get("/users", getUsers);
-router.post("/users", createUser);
-
+router.get("/home", renderHome);
 module.exports = router;
--- Admin/src/controladores/usuario.controlador.js
+++ Admin/src/controladores/usuario.controlador.js
@@ -1,10 +1,9 @@
-const { dataSource } = require("../ConfiguracionBaseDatos/appDataSource");
 const User = require("../modelos/User").User;
 const bcrypt = require("bcrypt");
 const saltRounds = 10;
 const userCtl = {};
 userCtl.renderHome = (req, res) => {
-  res.render("home");
+  res.render("/home");
 };
 
 userCtl.getUsers = async (req, res) => {
@@ -16,26 +15,6 @@ userCtl.getUsers = async (req, res) => {
   }
 };
 
-userCtl.getRegister = (req, res) => {
-  res.render("autenticacion/register");
-};
 
-userCtl.createUser = async (req, res) => {
-  try {
-    const { username, password, email, rols } = req.body;
-    dataSource.getRepository(User).create(req.body);
-    bcrypt.hash(password, saltRounds, async (err, hash) => {
-      const results = await dataSource.getRepository(User).save({
-        username,
-        email,
-        rols,
-        password: hash,
-      });
-      return res.json({ msg: "usuario creado", usuario: results });
-    });
-  } catch (error) {
-    console.log(error);
-  }
-};
 
 module.exports = userCtl;
